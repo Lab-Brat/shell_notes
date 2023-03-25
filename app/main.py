a35533edc38c198cd88db5ff63084e47a4dc74f3
Lab-Brat <xoovoox@protonmail.com>
2023-03-25 21:19:43 +0300

    add markdown syntax highlighting

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
+from rich.console import Console
+from rich.markdown import Markdown
 from .pathfinder import PathFinder
 
 app = FastAPI()
@@ -10,13 +12,20 @@ def reader(note):
         return f.read()
 
 
+def capture_console(note_string):
+    console = Console()
+    with console.capture() as capture:
+        console.print(Markdown(note_string))
+    return capture.get()
+
+
 @app.get("/", response_class=HTMLResponse)
 async def root(note: str):
     endpoints = PathFinder().all_paths
     for e in endpoints:
         try:
             e[note]
-            return reader(e[note])
+            return capture_console(reader(e[note]))
         except:
             pass
     return "Note not found :(\n"
